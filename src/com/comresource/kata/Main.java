bd6c9ea3a58a7bb0388994a68760190930b6bd19
David vollmar <david.bradley.vollmar@gmail.com>
2017-10-04 05:14:17 -0400

    Updated default Jframe icon with a custom image in Main. Added assets to img folder.

--- src/com/comresource/kata/Main.java
+++ src/com/comresource/kata/Main.java
@@ -1,12 +1,23 @@
 package com.comresource.kata;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Main extends JFrame {
 	public static void main(String[] args) {
 		JFrame window = new JFrame("Game of Life Kata");
 	    window.setContentPane(new KataView(6,8));  //rows, cols
-		window.setSize(1024,1024);
+		window.setSize(1024,1024);		
+		try {
+			window.setIconImage(ImageIO.read(new FileInputStream("img/cell.png")));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 		//window.setResizable(false);
